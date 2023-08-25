94767b9620c5e36de3ea1634441d6a4759f270f0
oktaykuzu <oktaykuzu00@gmail.com>
2023-08-25 16:22:14 +0300

    Hesap makinesi uygulaması

--- Hesapmakinesi/ViewController.swift
+++ Hesapmakinesi/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Ilktextim: UITextField!
+    @IBOutlet weak var Sonuclabel: UILabel!
+    @IBOutlet weak var Ikıncıtextim: UITextField!
+    
+    var sonuc = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      
     }
 
-
+    @IBAction func Toplamatiklandi(_ sender: Any) {
+        if let ilksayi=Int(Ilktextim.text!){
+            if let ikincisayi=Int(Ikıncıtextim.text!){
+                 sonuc = ilksayi + ikincisayi
+                Sonuclabel.text=String(sonuc)
+                
+            }
+        }
+       
+        
+    }
+    
+    @IBAction func CikarmaTiklandi(_ sender: Any) {
+        if let ilksayi=Int(Ilktextim.text!){
+            if let ikincisayi=Int(Ikıncıtextim.text!){
+                sonuc = ilksayi - ikincisayi
+                Sonuclabel.text=String(sonuc)
+                
+            }
+        }
+       
+        
+    }
+    
+    
+    @IBAction func CarpmayaTiklandi(_ sender: Any) {
+          if let ilksayi=Int(Ilktextim.text!){
+            if let ikincisayi=Int(Ikıncıtextim.text!){
+                sonuc = ilksayi * ikincisayi
+                Sonuclabel.text=String(sonuc)
+                
+            }
+        }
+       
+        
+    }
+    
+    
+    @IBAction func BolmeyeTiklandi(_ sender: Any) {
+        if let ilksayi=Int(Ilktextim.text!){
+            if let ikincisayi=Int(Ikıncıtextim.text!){
+                 sonuc = ilksayi / ikincisayi
+                Sonuclabel.text=String(sonuc)
+                
+            }
+        }
+       
+    }
+    
+    
 }
 
